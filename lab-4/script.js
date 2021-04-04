24433f7602c26ac0ddee234ab248d58864f7d546
Asaduzzaman Noor <mail-me@asaduzzaman-noor.com>
2021-04-04 21:44:01 +0600

    Finished!!
    Still can be improved, but I don't want to.

--- lab-4/script.js
+++ lab-4/script.js
@@ -1,9 +1,16 @@
-const form = document.querySelector(`#form`)
-
 const fullname = document.querySelector(`#fullname`),
       username = document.querySelector(`#username`),
       password = document.querySelector(`#password`),
-      repass   = document.querySelector(`#repass`)
+      repass   = document.querySelector(`#repass`),
+      gender   = document.querySelector(`#gender`),
+      mobile   = document.querySelector(`#contact`),
+      email    = document.querySelector(`#email`),
+      btn      = document.querySelector(`#btn-submit`)
+      fullname_regexp = /^[a-z .'-]+$/i,
+      username_regexp = /^[\S]+$/,
+      password_regexp = /^\S{8,32}$/,
+      mobile_regexp   = /^\d{11}$/,
+      email_regexp    = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/
 
 function indicator_status(element, pattern) {
     if(element.value.match(pattern)) {
@@ -27,25 +34,67 @@ function repass_check(pass1, pass2) {
 
 ;[fullname, username, password, repass].forEach(elem => {
     elem.addEventListener(`keyup`, (evt) => {
-	var regexp
 	switch(evt.target.id) {
 	    case `fullname`:
-		regexp = /^[a-z .'-]+$/i
-		break
+		indicator_status(evt.target, fullname_regexp)
+		return
 
 	    case `username`:
-		regexp = /^[\S]+$/
-		break
+		indicator_status(evt.target, username_regexp)
+		return
 
 	    case `password`:
-		regexp = /^\S{8,32}$/
-		break
+		indicator_status(evt.target, password_regexp)
+		return
 
 	    case `repass`:
 		repass_check(password.value, repass.value)
 		return
 	}
-	indicator_status(evt.target, regexp)
     })
 })
 
+
+btn.addEventListener(`click`, (evt) => {
+    evt.preventDefault()
+
+    error = false
+
+    if(!fullname.value.trim().match(fullname_regexp)) {
+	display_error(fullname, `Name can't be empty and alphabets, spaces, hyphens, apostrophes only`)
+	error = true
+    }
+
+    if(!username.value.trim().match(username_regexp)) {
+	display_error(username, `Please use alphanumeric characters, avoid space`)
+	error = true
+    }
+
+    if(!password.value.trim().match(password_regexp)) {
+	display_error(password, `Use 8-32 characters`)
+	error = true
+    }
+
+    if(password.value.trim() !== repass.value.trim()) {
+	display_error(repass, `Passwords don't match`)
+	error = true
+    }
+
+    if(!mobile.value.trim().match(mobile_regexp))
+	display_error(mobile, `Accepeted format: 01XXXXXXXXX`)
+
+    if(!email.value.trim().match(email_regexp))
+	display_error(email, `Please enter a valid email address`)
+
+    if (!error)
+	alert(`Good boy`)
+})
+
+function display_error(element, message) {
+    const parent_elem = element.parentElement,
+	  error_elem  = parent_elem.querySelector(`.block__error`)
+
+    console.log(error_elem)
+    error_elem.innerText = message
+    error_elem.style.visibility = `visible`
+}
